73c1e4f076e4de052ddab3f483f9aa99ca18c258
themumy10 <61433910+themumy10@users.noreply.github.com>
2023-03-13 16:49:39 +0300

    Version 1.0.3

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -14,10 +14,9 @@ const RESOURCES = {
 "splash/img/light-1x.png": "0cf5a19dedcac6c20416f23e2a1643a8",
 "splash/splash.js": "123c400b58bea74c1305ca3ac966748d",
 "splash/style.css": "c94c38ff00a9d487c353a2d78989ea08",
-"index.html": "3450c9540df3a446c0baf38fa415a2ad",
-"/": "3450c9540df3a446c0baf38fa415a2ad",
-"main.dart.js": "c2f3636073070eca4f75877cb0e93975",
-"flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
+"index.html": "56083988eab5e2de71ccfafc8705ec55",
+"/": "56083988eab5e2de71ccfafc8705ec55",
+"main.dart.js": "3750c31eaf1f7f38394d1fd13180f08a",
 "favicon.png": "eb4fff6abb172bda9a23e46951b5dbd7",
 "icons/favicon.ico": "8ee04cc280c557c4ff0065a3ffecca38",
 "icons/Icon-192.png": "817c54b3abb9d748196318840822294f",
@@ -110,11 +109,7 @@ const RESOURCES = {
 "assets/assets/fonts/Rubik-Bold.ttf": "f70066a21af08705d0503ad692446de1",
 "assets/assets/fonts/Rubik-Light.ttf": "98df4209c27b1be565511cc954fa307d",
 "assets/assets/fonts/Rubik-Medium.ttf": "bb476f36e32039a411d1f3afaf5a81af",
-"assets/assets/fonts/Rubik-Regular.ttf": "e100d91366c744a9fcf055b7c5af9961",
-"canvaskit/canvaskit.js": "97937cb4c2c2073c968525a3e08c86a3",
-"canvaskit/profiling/canvaskit.js": "c21852696bc1cc82e8894d851c01921a",
-"canvaskit/profiling/canvaskit.wasm": "371bc4e204443b0d5e774d64a046eb99",
-"canvaskit/canvaskit.wasm": "3de12d898ec208a5f31362cc00f09b9e"
+"assets/assets/fonts/Rubik-Regular.ttf": "e100d91366c744a9fcf055b7c5af9961"
 };
 
 // The application shell files that are downloaded before a service worker can
